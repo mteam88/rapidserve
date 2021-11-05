da315e5f37c4cc39a9ad085deb132687a7b0fb0c
mteam88 <matthew8edelen@gmail.com>
2021-11-05 19:39:20 +0000

    .

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ var express = require('express');
 var app = express();
 // Render static files
 app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 // Port website will run on
 app.listen(8080);
 
+// *** GET Routes - display pages ***
+// Root Route
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
 
 
 function encode256(number) {
