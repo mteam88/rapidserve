7e38394fd0afd400eb3c5d80bf262b7476a8b0ac
ZuniLunie <chotop3899@inst.hcpss.org>
2021-12-06 18:22:48 +0000

    .

--- public/js/connect.js
+++ public/js/connect.js
@@ -1,8 +1,16 @@
-// Use this how you may Cedric. Test run it with `node connect.js`
+// Use this how you may Cedric. Test run it with `node /workspace/rapidservehtml/public/js/connect.js`
 
-//const mysql = require('mysql');
+const mysql = require('mysql');
 const http = require('http');
 const fs = require('fs');
+
+var conpool = mysql.createPool({
+  host: 'remotemysql.com',
+  port: '3306',
+  user: '1PWxjfOccD',
+  password: '****', //Not real password talk to me at school if you have questions
+  database: '1PWxjfOccD'
+});
 //GetOrders();
 //CreateOrder(1, 10, "1");
 //DeleteOrder(1);
@@ -13,25 +21,16 @@ function CreateOrder(UserID, Order, Notes) {
   /*if (lastOrderDate == Math.floor(date)) {
     return {statusCode:2};
   }*/
+  
   var date = new Date();
 
   var Time = date.getTime();
-  var mysql = require('mysql');
-  var conpool = mysql.createPool({
-    host: 'remotemysql.com',
-    port: '3306',
-    user: '1PWxjfOccD',
-    password: 'JXLuoQC3xM', //Not real password talk to me at school if you have questions
-    database: '1PWxjfOccD'
-  });
-  alert(typeof conpool);
-
   conpool.getConnection(function(err) {
     if (err) throw err;
     var sql = `INSERT INTO orders (ID, OrderCode, Time, Notes) VALUES (${UserID}, ${Order}, ${Time}, ${Notes})`;
     conpool.query(sql, function (err, result) {
       if (err) throw err;
-      alert("1 record inserted");
+      console.log("1 record inserted");
     });
   });
 }
