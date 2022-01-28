3cd320c31c009e7de515729a08c81752f3d790df
mteam88 <matthew8edelen@gmail.com>
2022-01-28 18:58:49 +0000

    .

--- public/js/connect.js
+++ public/js/connect.js
@@ -1,5 +1,5 @@
 // Use this how you may Cedric. Test run it with `node /workspace/rapidservehtml/public/js/connect.js`
-
+return "b";
 const mysql = require('mysql');
 const http = require('http');
 const fs = require('fs');
@@ -54,3 +54,4 @@ xhttp.onload = function() {
 // Send a request
 xhttp.open("get", "https://3030-mteam88-rapidserver-3phd7aoe8mi.ws-us27.gitpod.io/public/js/connect.js");
 xhttp.send();
+}
