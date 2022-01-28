acaef5041db303dcdfdc58848af37df961d53131
mteam88 <matthew8edelen@gmail.com>
2022-01-28 18:37:53 +0000

    install xhr2

--- public/js/temp.js
+++ public/js/temp.js
@@ -1,11 +1,29 @@
 /*
 functions to interact with sql database
 */
+var XMLHttpRequest = require('xhr2');
+getOrder();
 function placeOrder() {
 
 }
 function getOrder() {
-    
+    var xhr = new XMLHttpRequest();
+    // we defined the xhr
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText);
+
+            alert(data);
+        }
+
+        // end of state change: it can be after some time (async)
+    };
+
+    xhr.open('GET', "https://3030-mteam88-rapidserver-3phd7aoe8mi.ws-us29.gitpod.io/public/js/connect.js", true);
+    xhr.send();
 }
 function editMenu() {
     
