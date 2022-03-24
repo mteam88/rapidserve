fc3c2a66a37ad49e3ea87dfb25aded7c802a6b7a
mteam88 <matthew8edelen@gmail.com>
2022-03-24 20:10:27 +0000

    cleared

--- public/js/authorize.js
+++
@@ -1,9 +0,0 @@
-function Authorize(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  var userInfo = {ID:profile.getId(),Full_Name:profile.getName(),Given_Name:profile.getGivenName(),Family_Name:profile.getFamilyName(),Image_URL:profile.getImageUrl(),Email:profile.getEmail(),ID_token:googleUser.getAuthResponse().id_token};
-  if (userInfo.Email.includes("@inst.hcpss.org")) {
-    return {statusCode:200};
-  } else {
-    return {statusCode:1};
-  }
-}
