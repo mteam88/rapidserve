a10f2b2b5c0dafe91b0f0da65780af6039686dc6
mteam88 <matthew8edelen@gmail.com>
2022-03-25 18:22:16 +0000

    checks if users account has been activated

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,9 @@
 module.exports = {
     ensureAuthenticated : function(req,res,next) {
     if(req.isAuthenticated()) {
-    return next();
+        if(req.user.active === true) {
+            return next();
+        }
     }
     req.flash('error_msg' , 'please login to use this');
     res.redirect('/profile/login');
