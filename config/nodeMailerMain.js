745315830bf3a14b165da69c7f805d526aba41a4
mteam88 <matthew8edelen@gmail.com>
2022-03-31 20:25:20 +0000

    wait until transporter created to run sendmail

--- config/nodeMailerMain.js
+++ config/nodeMailerMain.js
@@ -49,33 +49,32 @@ async function nodeMailerMain(targetEmail, targetName, hash) {
     //let testAccount = await nodemailer.createTestAccount().catch((err) => console.log(err))
     // create reusable transporter object using the default SMTP transport
 
-    let transporter = createTransporter(); //.catch((err) => console.log(err))
+    let transporter = createTransporter().then(() => {
+      //console.log(testAccount.smtp.host);
+      console.log("successfully created transporter");
 
+      let message = {
+          from: 'Tsla <tengtsla@gmail.com>',
+          to: targetName + ' <' + targetEmail + '>',
+          subject: targetName + ', Confirm your email for rapidserve',
+          text: 'Please confirm your email by clicking the following link:\n' + APPURL + '/profile/confirm/' + hash,
+          html: '<p><b>Hello' + targetName + '</b></p> <br> <p>Please confirm your email by clicking the following link:</p><br>' + APPURL + '/profile/confirm/' + hash
+      };
 
+      // send mail with defined transport object
+      transporter.sendMail(message, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Email sent: ' + info.response);
+          //console.log("Message sent: %s", info.messageId);
+          // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+          // Preview only available when sending through an Ethereal account
+          //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+          // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      });
+    }) //.catch((err) => console.log(err))
 
-    //console.log(testAccount.smtp.host);
-    console.log("successfully created transporter");
-
-    let message = {
-        from: 'Tsla <tengtsla@gmail.com>',
-        to: targetName + ' <' + targetEmail + '>',
-        subject: targetName + ', Confirm your email for rapidserve',
-        text: 'Please confirm your email by clicking the following link:\n' + APPURL + '/profile/confirm/' + hash,
-        html: '<p><b>Hello' + targetName + '</b></p> <br> <p>Please confirm your email by clicking the following link:</p><br>' + APPURL + '/profile/confirm/' + hash
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(message, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Email sent: ' + info.response);
-        //console.log("Message sent: %s", info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview only available when sending through an Ethereal account
-        //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
 }
 
 module.exports = nodeMailerMain
