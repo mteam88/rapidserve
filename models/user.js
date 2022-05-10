41f0c226da3644c3d70dcd808d675c0555aefc4d
mteam88 <matthew8edelen@gmail.com>
2022-05-10 16:13:19 +0000

    add permissions section to schema

--- models/user.js
+++ models/user.js
@@ -1,26 +1,39 @@
 const mongoose = require('mongoose');
+
+const PermissionsSchema  = new mongoose.Schema({
+  canvieworders : {type : Boolean, required : true},
+  caneditmenu : {type : Boolean, required : true},
+  caneditperms : {type : Boolean, required : true},
+});
+
+
 const UserSchema  = new mongoose.Schema({
-  name :{
+  name: {
       type  : String,
       required : true
   } ,
-  active :{
+  active: {
     type: Boolean,
     default: false
   },
-  email :{
+  email: {
     type  : String,
     required : true
-} ,
-password :{
-    type  : String,
+  },
+  password: {
+    type : String,
     required : true
-} ,
-date :{
+  },
+  date: {
     type : Date,
     default : Date.now
-}
+  },
+  permissions: {
+    type: PermissionsSchema,
+    required: true
+  }
 });
+
 const User= mongoose.model('User',UserSchema);
 
 module.exports = User;
