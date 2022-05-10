49c15bb3696fcd9b82bd64aaeb878287c16faf8f
mteam88 <matthew8edelen@gmail.com>
2022-05-10 17:51:24 +0000

    various

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const PermissionsSchema  = new mongoose.Schema({
 
 const UserSchema  = new mongoose.Schema({
   name: {
-      type  : String,
+      type: String,
       required : true
   } ,
   active: {
@@ -21,11 +21,7 @@ const UserSchema  = new mongoose.Schema({
     required : true
   },
   password: {
-<<<<<<< HEAD
-    type : String,
-=======
     type  : String,
->>>>>>> 2a3608e6ee284dacb27973c84d70d7d87a9ac050
     required : true
   },
   date: {
@@ -33,15 +29,8 @@ const UserSchema  = new mongoose.Schema({
     default : Date.now
   },
   permissions: {
-<<<<<<< HEAD
     type: PermissionsSchema,
     required: true
-=======
-    required : true,
-    canvieworders : {type : Boolean, required : true},
-    caneditmenu : {type : Boolean, required : true},
-    caneditperms : {type : Boolean, required : true}
->>>>>>> 2a3608e6ee284dacb27973c84d70d7d87a9ac050
   }
 });
 
